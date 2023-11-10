d3b989869f754312ee55c846e780c65af82dcae4
mustafayumrutepe <mstfyumrutepe@gmail.com>
2023-11-10 04:49:21 +0300

    jenkins Chrome ve Hooks after  ayarları yapıldı

--- src/test/java/StepDefinitions/Hooks.java
+++ src/test/java/StepDefinitions/Hooks.java
@@ -17,13 +17,15 @@ public class Hooks {
     @After   //cucumber in annotation i
     public void after(Scenario senaryo){
 
-        ExcelUtility.writeToExcel("src/test/java/ApachePOI/resource/SenaryoSonuclari3.xlsx",senaryo);
-        // extent reportun plugini aktif iken açık kalsın //yani mesela _07_TestRunnerExtentReport taki gibi  plugin = {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"} varsa çalışsın, bu yoksa burayı kapatalım. yoksa hata veriri, plugin demek rapor demek
-        if (senaryo.isFailed()){
-            TakesScreenshot ts=((TakesScreenshot) GWD.getDriver());
-            byte[] hafizadakiHali=ts.getScreenshotAs(OutputType.BYTES);
-            senaryo.attach(hafizadakiHali, "image/png", "screenshot name");
-        }
+//        ExcelUtility.writeToExcel("src/test/java/ApachePOI/resource/SenaryoSonuclari3.xlsx",senaryo);
+//        // extent reportun plugini aktif iken açık kalsın //yani mesela _07_TestRunnerExtentReport taki gibi  plugin = {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"} varsa çalışsın, bu yoksa burayı kapatalım. yoksa hata veriri, plugin demek rapor demek
+//        if (senaryo.isFailed()){
+//            TakesScreenshot ts=((TakesScreenshot) GWD.getDriver());
+//            byte[] hafizadakiHali=ts.getScreenshotAs(OutputType.BYTES);
+//            senaryo.attach(hafizadakiHali, "image/png", "screenshot name");
+//        }
+//
+//        (bu  kısmı jenkins için kapattık)
 
         GWD.quitDriver();
     }
